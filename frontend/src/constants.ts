49dc573faf8bc5faef05f7effeb9c379a44f11e4
Timoha Vysochynskyi <timohavysach@gmail.com>
2025-02-01 23:04:23 +0200

    fix: constants

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 //export const apiDomain = 'http://localhost:3000'
-=======
-// export const apiDomain = 'http://localhost:3000'
->>>>>>> 9bbbd9158d7dc147691cb1d6736766f625d50004
 export const apiDomain = 'https://tactiforge.onrender.com'
