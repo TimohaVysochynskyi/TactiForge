082fc9dc493d3c2bfce4285bcb4ff73992c86719
Timoha Vysochynskyi <timohavysach@gmail.com>
2025-04-09 23:15:38 +0300

    feat: CustomLoader

--- frontend/src/components/CustomLoader/CustomLoader.tsx
+++ frontend/src/components/CustomLoader/CustomLoader.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+import Loader from "../Loader/Loader";
 import css from "./CustomLoader.module.css";
 
 export default function CustomLoader() {
-  return <></>;
+  const messages = [
+    "Готуємо зброю...",
+    "Підготовлюємо розмітку...",
+    "Завантажуємо теорію...",
+    "Робимо запит на базу даних...",
+  ];
+
+  const [currentMessage, setCurrentMessage] = useState<number>(0);
+
+  function handleMessageChange() {
+    if (currentMessage >= 3) setCurrentMessage(0);
+    setCurrentMessage(currentMessage + 1);
+  }
+  setInterval(handleMessageChange, 3500);
+
+  return (
+    <>
+      <div className={css.container}>
+        <div className={css.contentWrapper}>{messages[currentMessage]}</div>
+        <Loader size="80" position="absolute" />
+      </div>
+    </>
+  );
 }
